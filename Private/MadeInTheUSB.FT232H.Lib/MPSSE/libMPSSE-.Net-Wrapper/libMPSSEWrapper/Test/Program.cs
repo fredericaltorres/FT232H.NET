a6cc2523850475cf452b9ca559fc5082c002b72c
BRAINSHARK\ftorres <frederic.torres@bigtincan.com>
2023-03-25 14:48:58 -0400

    Added FTDI libmpsse source code and compile it

---
+++ Private/MadeInTheUSB.FT232H.Lib/MPSSE/libMPSSE-.Net-Wrapper/libMPSSEWrapper/Test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using libMPSSEWrapper;
+using libMPSSEWrapper.Types;
+
+namespace Test
+{
+    class Program
+    {
+        private const int ConnectionSpeed = 2000; // Hz
+        private const int LatencyTimer = 255; // Hz
+
+        static void Main(string[] args)
+        {
+
+            var adcSpiConfig = new FtChannelConfig
+                             {
+                                 ClockRate = ConnectionSpeed,
+                                 LatencyTimer = LatencyTimer,
+                                 configOptions = FtConfigOptions.Mode0 | FtConfigOptions.CsDbus3 | FtConfigOptions.CsActivelow
+                             };
+
+          
+
+            var adcConfig = new Maxim186Configuration
+                                {
+                                    Channel = Maxim186.Channel.Channel0,
+                                    ConversionType = Maxim186.ConversionType.SingleEnded,
+                                    Polarity = Maxim186.Polarity.Unipolar,
+                                    PowerMode = Maxim186.PowerMode.InternalClockMode
+                                };
+
+           
+
+            var adc = new Maxim186(adcConfig, adcSpiConfig);
+
+            do
+            {
+                Console.WriteLine(adc.GetConvertedSample());
+
+               
+
+            } while (true);
+            //*/
+        }
+    }
+}
